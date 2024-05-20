10b7356a54de2fe6de6079bb4959048340069175
ZEXO <162705072+ELMUTAZ-ABDELRAHMAN@users.noreply.github.com>
2024-05-20 21:01:49 +0400

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,22 @@ from translatepy.translators.reverso import ReversoTranslate
 from translatepy.translators.translatecom import TranslateComTranslate
 from translatepy.translators.microsoft import MicrosoftTranslate
 from translatepy.translators.deepl import DeeplTranslateException
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
